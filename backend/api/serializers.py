791b1212e70624fb73544f39abd2117999815093
Ajay v <vajay4834@gmail.com>
2024-01-23 16:08:51 +0530

    seralizer modified

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,13 +7,29 @@ class ProductSerializer(serializers.ModelSerializer):
         model=Products
         fields="__all__"
 class UserSeralizer(serializers.ModelSerializer):
+    name=serializers.SerializerMethodField(read_only=True)
+    _id=serializers.SerializerMethodField(read_only=True)
+    isAdmin=serializers.SerializerMethodField(read_only=True)
     class Meta:
         model=User
-        fields='__all__'
+        fields=['id','_id','username','email','name','isAdmin']
+    def get_name(self,obj):
+        firstname=obj.first_name    
+        lastname=obj.last_name 
+        name=firstname+' '+lastname
+        if name==' ':
+            name='Set Name'
+        return name
+    def get__id(self,obj):
+        return obj.id
+    def get_isAdmin(self,obj):
+        return obj.is_staff
+
 class UserSeralizerWithToken(serializers.ModelSerializer):
+    token=serializers.SerializerMethodField(read_only=True)
     class Meta:
         model=User
-        fields='__all__'
+        fields=['id','_id','username','email','name','isAdmin','token']
 
     def get_token(self,obj):
         token=RefreshToken.for_user(obj)
