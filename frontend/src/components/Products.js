1da4ed3fb53abb970a04e985191b61fb78e38399
Ajay v <vajay4834@gmail.com>
2024-01-24 13:34:59 +0530

    styles changed

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -7,26 +7,28 @@ const Products = ({product}) => {
   return (
     <>
       <Link to={`/product/${product._id}`}>
-      <Card
-        className="card  text-white bg-primary mb-3  rounded-4"
-        style={{ width: "40rem" }}
+      <div
+        className="card   bg-primary m-3 p-2 rounded-4"
+        style={{ width: "20rem" }}
       >
-        <Col key={product._id} sm={12} lg={4} xl={3} md={6} className="m-3">
+       
           <img
             src={product.image}
-            className=" "
+            className="card-img-top "
             alt="none"
             style={{ width: "10rem", height: "300px", objectFit: "contain" }}
           />
-          <h3>{product.productname}</h3>
+          <div className="card-body">
+          <Card.Title>{product.productname}</Card.Title>
           <p>{product.brand}</p>
           
-          <p>{product.info}</p>
+          <Card.Text className="overflow-hidden text-justify">{product.info}</Card.Text>
           <Rating value={product.rating}
           text={`${product.numReviews} reviews`}
           color={"#f8e825"}/>
-        </Col>
-      </Card>
+          </div>
+      
+      </div>
       </Link>
     </>
   );
